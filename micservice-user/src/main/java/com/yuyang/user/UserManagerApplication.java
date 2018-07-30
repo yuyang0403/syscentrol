26cba7fb67279f2766ca32b9cf67a0874d2f378e
yuyang <yuyang@winchannel.net>
2018-07-30 18:19:26 +0800

    配置中心完成

--- micservice-user/src/main/java/com/yuyang/user/UserManagerApplication.java
+++ micservice-user/src/main/java/com/yuyang/user/UserManagerApplication.java
@@ -1,23 +1,14 @@
 package com.yuyang.user;
 
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableAutoConfiguration
+@SpringCloudApplication
 @MapperScan("com.yuyang.user.mapper")
 public class UserManagerApplication {
     public static void main(String[] args) {
--- micservice-zuul/src/main/java/com/yuyang/zuul/MicserviceZuulApplication.java
+++ micservice-zuul/src/main/java/com/yuyang/zuul/MicserviceZuulApplication.java
@@ -3,11 +3,13 @@ package com.yuyang.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableZuulProxy
+@SpringCloudApplication
 public class MicserviceZuulApplication {
 
     public static void main(String[] args) {
