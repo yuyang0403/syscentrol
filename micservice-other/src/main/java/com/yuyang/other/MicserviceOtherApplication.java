b2212cb379a694a276854ba52d6f9a7dffecd6b4
yuyang <yuyang@winchannel.net>
2018-08-27 11:00:29 +0800

    添加项目，各种技术的应用

---
+++ micservice-other/src/main/java/com/yuyang/other/MicserviceOtherApplication.java
@@ -0,0 +1,12 @@
+package com.yuyang.other;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicserviceOtherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicserviceOtherApplication.class, args);
+    }
+}
